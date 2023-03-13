1694d2043e2f199e6b8aa53d49d68e2566e1c77b
Luai <unitools771@gmail.com>
2023-03-13 15:59:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// var x = prompt("Enter 1st Number")
+// var y = prompt("Enter 2nd Number")
+
+// if(x > y && x!=y){
+//     alert("The Max Num is : "+ x)
+// }else if(x < y && x!=y)
+// {
+//     alert("The Max Num is : "+ y)
+// }else{
+//     alert("Pls Enter valid Data")
+// }  
+////////////////////////////////////////////////////////////////////////////
+
+// var x = prompt("Enter 1st Number")
+// var y = prompt("Enter 2nd Number")
+// var z = prompt("Enter 3rd Number")
+// if(x > y && x!=y){
+//     if(x>z && x!=z){
+//      alert("The Max Num is : "+ x)
+//     }else{
+//         alert("The Max Num is : "+ z)
+//     }
+// } else if(y > x && y!=x ){
+//     if(y > z && y!=z){
+//         alert("The Max Num is : "+ y)
+//        }else{
+//            alert("The Max Num is : "+ z)
+//        }
+// }
+///////////////////////////////////////////////////////////////////////////
+
+// var x = prompt("Enter 1st Number")
+// var y = prompt("Enter 2nd Number")
+// var z = prompt("Enter 3rd Number")
+
+// if(x!=y || x!=z || y!=z){
+//         let arr =[x, y, z]
+//     var max = Math.max(x, y, z)
+//     var min = Math.min(x, y, z)
+//     alert("The Min is : "+min + "  &  The Max is : "+max)
+// }else{
+//     alert(" Duplicate Entery ")
+// }
+
+/////////////////////////////////////////////////////////////////////////////
+
+// let values = prompt("Enter values sep by , ") 
+// let arr = values.split(",")
+// z = 0
+// for(i=0; i<arr.length; i++){
+//     z += parseInt(arr[i]) 
+// }
+// var avg = z / arr.length
+// alert("Total Marks is : "+z +"  &  The Average is : "+avg)
+
+////////////////////////////////////////////////////////////////////////////
+ 
+
+// let text = prompt("Enter Text")
+// let arr = text.split("")
+// for(i=0; i < arr.length; i++){
+//     // console.log(arr[i])
+//     if(arr[i] == arr[i].toUpperCase()){
+//         arr[i] = arr[i].toLowerCase()
+//         // console.log("Ori UpperCase")
+//     }else{
+//         arr[i] = arr[i].toUpperCase()
+//         // console.log("Ori LowerCase")
+//     }    
+// }
+// console.log(arr)
